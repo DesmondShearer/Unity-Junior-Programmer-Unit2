fb3f99856a59a66608eabc6ee2b5f45d98756a8f
Des Shearer <79203711+DesmondShearer@users.noreply.github.com>
2025-09-26 16:53:42 +0100

    added destroy to dog prefabs
    bottomBound -10

--- Unit2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Unit2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30.0f;
+    private float bottomBound = -10.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +14,9 @@ public class DestroyOutOfBounds : MonoBehaviour
     void Update()
     {
         if (transform.position.z > topBound)
+        {
+            Destroy(this.gameObject);
+        } else if (transform.position.z < bottomBound)
         {
             Destroy(this.gameObject);
         }
